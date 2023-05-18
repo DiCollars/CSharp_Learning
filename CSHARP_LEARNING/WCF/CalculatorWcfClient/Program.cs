216ce5967172f8db1c143408264321d514942efa
Daniyal Isakhanov <isakhanov@reksoft.ru>
2023-05-18 22:49:48 +0300

    I have revised wcf

---
+++ CSHARP_LEARNING/WCF/CalculatorWcfClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CalculatorWcfClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            var result = client.Add(5.5, 1.0);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSHARP_LEARNING/WCF/WcfExample/IService1.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceModel;
+
+namespace WcfExample
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        double Add(double firstVal, double secondVal);
+
+        [OperationContract]
+        double Sub(double firstVal, double secondVal);
+
+        [OperationContract]
+        double Mul(double firstVal, double secondVal);
+
+        [OperationContract]
+        double Div(double firstVal, double secondVal);
+    }
+}
